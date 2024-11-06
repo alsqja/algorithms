e28d85d7e91d236da0b3e73ca22b707d443c88db
alsqja <alsqha2626@gmail.com>
2024-11-06 10:24:55 +0900

    add점찍기

---
+++ src/programmers연속부분수열합의개수/Solution.java
@@ -0,0 +1,7 @@
+package programmers연속부분수열합의개수;
+
+public class Solution {
+    public int solution(int[] elements) {
+        return 0;
+    }
+}
---
+++ src/programmers점찍기/Solution.java
@@ -0,0 +1,14 @@
+package programmers점찍기;
+
+public class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        long x = (long) d * d;
+
+        for (long i = 0; i <= d; i += k) {
+            answer += (long) (Math.sqrt(x - i * i)) / k + 1;
+        }
+
+        return answer;
+    }
+}
