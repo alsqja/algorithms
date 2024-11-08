f4149a59af9ddf8c1d9b5e1b7ce549607cf6aedb
alsqja <alsqha2626@gmail.com>
2024-11-08 09:18:03 +0900

    add 연속부분수열합의개수

--- src/programmers연속부분수열합의개수/Solution.java
+++ src/programmers연속부분수열합의개수/Solution.java
@@ -1,7 +1,25 @@
 package programmers연속부분수열합의개수;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int solution(int[] elements) {
-        return 0;
+        int[] arr = new int[elements.length * 2];
+
+        for (int i = 0; i < elements.length; i++) {
+            arr[i] = elements[i];
+            arr[i + elements.length] = elements[i];
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+                set.add(Arrays.stream(arr, j, j + i).sum());
+            }
+        }
+
+        return set.size();
     }
 }
