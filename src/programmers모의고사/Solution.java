137ca2f6a8a0b1b97d99ba6cfb3b9433931611c1
alsqja <alsqha2626@gmail.com>
2025-02-26 14:25:26 +0900

    add 모의고사

--- src/programmers모의고사/Solution.java
+++ src/programmers모의고사/Solution.java
@@ -1,51 +1,46 @@
 package programmers모의고사;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public int[] solution(int[] answers) {
+
         int[] a = {1, 2, 3, 4, 5};
         int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int aCount = 0;
-        int bCount = 0;
-        int cCount = 0;
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
 
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == a[i % 5]) {
-                aCount++;
+                count1++;
             }
             if (answers[i] == b[i % 8]) {
-                bCount++;
+                count2++;
             }
             if (answers[i] == c[i % 10]) {
-                cCount++;
+                count3++;
             }
         }
 
-        int max = Math.max(Math.max(aCount, bCount), cCount);
+        List<Integer> counts = new ArrayList<>();
+        counts.add(count1);
+        counts.add(count2);
+        counts.add(count3);
 
-        List<Integer> list = new ArrayList<>();
-        if (aCount == max) {
-            list.add(1);
+        List<Integer> ans = new ArrayList<>();
+
+        int max = counts.stream().max(Integer::compareTo).get();
+
+        for (int i = 0; i < 3; i++) {
+            if (counts.get(i) == max) {
+                ans.add(i + 1);
+            }
         }
-        if (bCount == max) {
-            list.add(2);
-        }
-        if (cCount == max) {
-            list.add(3);
-        }
 
-        return list.stream().mapToInt(i -> i).toArray();
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] answers = {1, 3, 2, 4, 2};
-
-        System.out.println(Arrays.toString(solution.solution(answers)));
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 }
