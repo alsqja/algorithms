73f6e41dfaa927e26f9916669619344dcbc92faa
alsqja <alsqha2626@gmail.com>
2025-03-03 14:43:33 +0900

    add 옹알이2

--- src/programmers옹알이2/Solution.java
+++ src/programmers옹알이2/Solution.java
@@ -2,24 +2,25 @@ package programmers옹알이2;
 
 public class Solution {
     public int solution(String[] babblings) {
-        // "aya", "ye", "woo", "ma" 4가지 발음만 가능
+
         int answer = 0;
-        for (int i = 0; i < babblings.length; i++) {
-            if (babblings[i].contains("ayaaya") || babblings[i].contains("yeye") || babblings[i].contains("woowoo") || babblings[i].contains("mama")) {
+
+        String[] words = new String[]{"aya", "ye", "woo", "ma"};
+
+        for (String babbling : babblings) {
+            if (babbling.contains("ayaaya") || babbling.contains("yeye") || babbling.contains("woowoo") || babbling.contains("mama")) {
                 continue;
             }
 
-            babblings[i] = babblings[i].replace("aya", " ");
-            babblings[i] = babblings[i].replace("ye", " ");
-            babblings[i] = babblings[i].replace("woo", " ");
-            babblings[i] = babblings[i].replace("ma", " ");
-            babblings[i] = babblings[i].replace(" ", "");
-
-            if (babblings[i].isEmpty()) {
-                answer++;
+            for (String word : words) {
+                babbling = babbling.replaceAll(word, " ");
             }
 
+            if (babbling.trim().isEmpty()) {
+                answer++;
+            }
         }
+
         return answer;
     }
 }
